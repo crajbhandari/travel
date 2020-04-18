d2506c3df7d76cbb4fb748ed0fc55269e87840e8
Bipin Joshi <joshibipin2052@gmail.com>
2020-04-18 22:58:56 +1000

    some corrections in package form

--- common/components/HelperPackage.php
+++ common/components/HelperPackage.php
@@ -88,6 +88,7 @@ class HelperPackage extends Component {
                     $model->images = $final;
                 }
                 else {
+
                     if (!($model->save() == false)) {
                         Misc::setFlash('success', 'Package Updated.');
                         return $var = [
